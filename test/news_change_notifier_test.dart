3147bbfd142c84a5fd243071c6cfbf499b194d04
Novjean J Kannathara <novjeanj@gmail.com>
2024-06-06 23:53:43 +0530

    group unit test

--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -24,4 +24,39 @@ void main() {
       expect(sut.isLoading, false);
     },
   );
+
+  group('getArticles', () {
+    final articlesFromService = [Article(title: 'Test 1', content: 'Test 1 content'),
+      Article(title: 'Test 2', content: 'Test 2 content'),
+      Article(title: 'Test 3', content: 'Test 3 content'),];
+
+    void arrangeNewsServiceReturns3Articles() {
+      when(() => mockNewsService.getArticles()).thenAnswer((_) async => articlesFromService);
+    }
+
+    test(
+      "gets articles using the NewsService",
+      () async {
+        // when(() => mockNewsService.getArticles()).thenAnswer((_) async => []);
+        arrangeNewsServiceReturns3Articles();
+
+        await sut.getArticles();
+        verify(() => mockNewsService.getArticles()).called(1);
+      },
+    );
+
+    test(
+      """indicates loading of data, 
+      sets articles to the ones from the service,
+      indicates that data is not being loaded anymore""",
+          () async {
+        arrangeNewsServiceReturns3Articles();
+        final future = sut.getArticles(); // future here so that we are not awaiting and to see if isLoading below is correct
+        expect(sut.isLoading, true);
+        await future;
+        expect(sut.articles, articlesFromService);
+        expect(sut.isLoading, false);
+      },
+    );
+  });
 }
